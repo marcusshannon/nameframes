b052bcf5df4d53327caebe4184e817a335ff0a02
Marcus Shannon <mpshannon@me.com>
2020-03-29 22:42:22 -0400

    live lobby data

--- lib/nameframes_web/live/lobby_component.ex
+++ lib/nameframes_web/live/lobby_component.ex
@@ -10,12 +10,15 @@ defmodule NameframesWeb.LobbyComponent do
     player_names = Games.player_names(assigns.game)
     is_host = Games.is_host?(assigns.game, assigns.player_id)
     can_start_game? = is_host and assigns.game.status === :lobby
+    host_id = assigns.game.host
+    host_name = assigns.game.players[host_id].name
 
     {:ok,
      socket
      |> Map.put(:assigns, assigns)
      |> assign(:player_names, player_names)
      |> assign(:is_host, is_host)
-     |> assign(:can_start_game?, can_start_game?)}
+     |> assign(:can_start_game?, can_start_game?)
+     |> assign(:host_name, host_name)}
   end
 end
